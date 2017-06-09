7bdd5eab0003d1ee3011d9d9dae44fcf54bd692f
Armaël Guéneau <armael.gueneau@ens-lyon.fr>
2017-06-09 09:42:17 +0200

    Add a reimplementation of i3-msg as an example

---
+++ examples/i3_msg.ml
@@ -0,0 +1,52 @@
+let message_type = ref "command"
+let payload_elts = ref []
+
+let _ = Arg.parse [
+  "-t", Arg.Set_string message_type, "Type of the message to send. By default, \"command\". Supported types: command, get_workspaces, get_outputs, get_tree, get_marks, get_bar_config, get_version"
+]
+  (fun s -> payload_elts := s :: !payload_elts)
+  "i3-msg: send an IPC message to i3"
+
+(* Use all arguments, separated by whitespace, as payload *)
+let payload = String.concat " " !payload_elts
+
+let pp_list fmt pp l =
+  let rec aux = function
+    | [] -> ()
+    | [x] -> pp x
+    | x::y::xs ->
+      pp x; Format.fprintf fmt ","; Format.pp_print_space fmt (); aux (y :: xs)
+  in
+  Format.fprintf fmt "["; aux l; Format.fprintf fmt "]"
+
+let main =
+  let fmt = Format.std_formatter in
+  let%lwt conn = I3ipc.connect () in
+  match !message_type with
+  | "command" ->
+    let%lwt outcome = I3ipc.command conn payload in
+    pp_list fmt (I3ipc.Reply.pp_command_outcome fmt) outcome |> Lwt.return
+  | "get_workspaces" ->
+    let%lwt workspaces = I3ipc.get_workspaces conn in
+    pp_list fmt (I3ipc.Reply.pp_workspace fmt) workspaces |> Lwt.return
+  | "get_outputs" ->
+    let%lwt outputs = I3ipc.get_outputs conn in
+    pp_list fmt (I3ipc.Reply.pp_output fmt) outputs |> Lwt.return
+  | "get_tree" ->
+    let%lwt tree = I3ipc.get_tree conn in
+    I3ipc.Reply.pp_node fmt tree |> Lwt.return
+  | "get_marks" ->
+    let%lwt marks = I3ipc.get_marks conn in
+    pp_list fmt (Format.pp_print_string fmt) marks |> Lwt.return
+  | "get_bar_ids" ->
+    let%lwt bar_ids = I3ipc.get_bar_ids conn in
+    pp_list fmt (Format.pp_print_string fmt) bar_ids |> Lwt.return
+  | "get_bar_config" ->
+    let%lwt bar_cfg = I3ipc.get_bar_config conn payload in
+    I3ipc.Reply.pp_bar_config fmt bar_cfg |> Lwt.return
+  | "get_version" ->
+    let%lwt version = I3ipc.get_version conn in
+    I3ipc.Reply.pp_version fmt version |> Lwt.return
+  | _ -> Format.fprintf Format.err_formatter "Unsupported message type"; exit 1
+
+let () = Lwt_main.run main
--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -1,5 +1,5 @@
 (* OASIS_START *)
-(* DO NOT EDIT (digest: 9e4eecb9a74de7b97e7d3cfb3ae6258c) *)
+(* DO NOT EDIT (digest: e5c3a87335344f8d569bacdaa5607aca) *)
 module OASISGettext = struct
 (* # 22 "src/oasis/OASISGettext.ml" *)
 
@@ -105,10 +105,7 @@ module OASISString = struct
         ok := false;
       incr str_idx
     done;
-    if !what_idx = String.length what then
-      true
-    else
-      false
+    !what_idx = String.length what
 
 
   let strip_starts_with ~what str =
@@ -131,10 +128,7 @@ module OASISString = struct
         ok := false;
       decr str_idx
     done;
-    if !what_idx = -1 then
-      true
-    else
-      false
+    !what_idx = -1
 
 
   let strip_ends_with ~what str =
@@ -440,7 +434,7 @@ module OASISExpr = struct
 end
 
 
-# 443 "myocamlbuild.ml"
+# 437 "myocamlbuild.ml"
 module BaseEnvLight = struct
 (* # 22 "src/base/BaseEnvLight.ml" *)
 
@@ -520,7 +514,7 @@ module BaseEnvLight = struct
 end
 
 
-# 523 "myocamlbuild.ml"
+# 517 "myocamlbuild.ml"
 module MyOCamlbuildFindlib = struct
 (* # 22 "src/plugins/ocamlbuild/MyOCamlbuildFindlib.ml" *)
 
@@ -881,14 +875,14 @@ module MyOCamlbuildBase = struct
 end
 
 
-# 884 "myocamlbuild.ml"
+# 878 "myocamlbuild.ml"
 open Ocamlbuild_plugin;;
 let package_default =
   {
      MyOCamlbuildBase.lib_ocaml = [("i3ipc", ["src"], [])];
      lib_c = [];
      flags = [];
-     includes = []
+     includes = [("examples", ["src"])]
   }
   ;;
 
@@ -896,6 +890,6 @@ let conf = {MyOCamlbuildFindlib.no_automatic_syntax = false}
 
 let dispatch_default = MyOCamlbuildBase.dispatch_default conf package_default;;
 
-# 900 "myocamlbuild.ml"
+# 894 "myocamlbuild.ml"
 (* OASIS_STOP *)
 Ocamlbuild_plugin.dispatch dispatch_default;;
